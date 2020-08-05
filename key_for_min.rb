05ab21207864dfd82823f3e6d14ce98f48e691aa
Alex Mata <alexmata92@ymail.com>
2020-08-05 21:09:23 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,3 +9,19 @@ def key_for_min_value(name_hash)
   end
  name_value
 end
+
+
+#comparison code I received from Ashab
+#def key_for_min_value(name_hash)
+ #lowest_value = nil   
+ #lowest_key = nil 
+  #name_hash.each do |key, value|
+    
+     #if lowest_value == nil || value < lowest_value
+      #lowest_value = value
+      #lowest_key = key
+      
+    #end
+  #end
+  #lowest_key
+#end
